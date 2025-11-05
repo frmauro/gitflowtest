1b1eddac5d4f6911b6f4dd81ba76627621e1ca5d
frmauro <frmauro8@gmail.com>
2025-11-05 18:09:49 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// main.js
+// Imprime uma mensagem de boas-vindas no console
+
+function boasVindas(nome = 'visitante') {
+	console.log(`Bem-vindo(a), ${nome}! Seja bem-vindo(a) ao programa.`);
+}
+
+// Exemplo de uso
+boasVindas('usuário');
+
