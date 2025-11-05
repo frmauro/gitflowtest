de8ea85c214beedd9753535df5cc6928ef3f92c8
frmauro <frmauro8@gmail.com>
2025-11-05 19:34:20 -0300

    many changes 001

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 function boasVindas(nome = 'visitante') {
 	console.log(`Bem-vindo(a), ${nome}! Seja bem-vindo(a) ao programa teste!`);
     console.log('Esta é uma mensagem adicional para demonstrar a funcionalidade.');
+    console.log('Esta é uma linha extra adicionada ao código.');
 }
 
 // Exemplo de uso
