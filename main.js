b4fe31854425b936a6db562ee7c127c471120e55
frmauro <frmauro8@gmail.com>
2025-11-13 17:36:25 -0300

    CHANGE 02

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 function boasVindas(nome = 'visitante') {
 	console.log(`Bem-vindo(a), ${nome}! Seja bem-vindo(a) ao programa teste!`);
     console.log('Esta é uma mensagem adicional para demonstrar a funcionalidade.');
-    console.log('Esta é uma linha xxxxxxxx extra adicionada ao código.');
+    console.log('Esta é uma linha xxxxxxxxuuuuuuu extra adicionada ao código.');
 }
 
 // Exemplo de uso
